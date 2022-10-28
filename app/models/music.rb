5214dbc029b46c72e9b4e7da8b4875b42f336ee9
Amayaqing <amayaqing@gmail.com>
2022-10-27 22:02:04 -0400

    add more data in seeds

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,11 +1,11 @@
 class Music < ActiveRecord::Base
+
   def self.all_categories
     ['Rock', 'Country', 'Pop', 'Blues', 'Classical', 'Jazz']
   end
-  def self.with_categories(categories, sort_by)
-    
-      
-      all.order sort_by
 
+  def self.with_categories(categories, sort_by)
+      all.order sort_by
   end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 musics = [
-  #{ title: 'Love story', category: 'Country', release_date: '25-Nov-1992', singer: 'Taylor Swift', description: "Lovely music by Taylor" }
+  { title: 'Love story Country', category: 'Country', release_date: '25-Nov-1992', singer: 'Taylor Swift', description: "Lovely music by Taylor" },
+  { title: 'Love story Rock', category: 'Rock', release_date: '25-Nov-1992', singer: 'Taylor Swift', description: "Lovely music by Taylor" },
+  { title: 'Love story Pop', category: 'Pop', release_date: '25-Nov-1992', singer: 'Taylor Swift', description: "Lovely music by Taylor" },
+  { title: 'Love story Blues', category: 'Blues', release_date: '25-Nov-1992', singer: 'Taylor Swift', description: "Lovely music by Taylor" },
+  { title: 'Love story Classical', category: 'Classical', release_date: '25-Nov-1992', singer: 'Taylor Swift', description: "Lovely music by Taylor" },
+  { title: 'Love story Jazz', category: 'Jazz', release_date: '25-Nov-1992', singer: 'Taylor Swift', description: "Lovely music by Taylor" },
+
 ]
 
 musics.each do |music|
