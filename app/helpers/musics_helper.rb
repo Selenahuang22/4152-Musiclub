e258e4214bf286be1888a82f82e5a811a18867b6
Amayaqing <amayaqing@gmail.com>
2022-11-14 22:46:10 -0500

    layout and picture

--- app/helpers/musics_helper.rb
+++ app/helpers/musics_helper.rb
@@ -1,6 +1,20 @@
 module MusicsHelper
   # Checks if a number is odd:
+
+
   def oddness(count)
     count.odd? ?  "odd" :  "even"
+  end
+
+  def musicpic(singer)
+    path = "/images/"
+    pic = ["Alan Walker", "Alexandra Kay", "Avril Lavigne", "Beethoven", "Bizet", "Bruce Springsteen", "Charles Brown", "Charley Patton", "Don Henley", "Eagles", "Ella Fitzgerald", "Frank Sinatra", "J.S. Bach", "Louis Armstrong", "Maroon 5", "Miles Davis", "Monteverdi", "Morgan Wallen", "Mozart", "Muddy Waters", "Otis Rush", "Taylor Swift", "Troye Sivan", "Vivaldi"]
+
+    if pic.include?(singer)
+      picname = singer+".jpg"
+    else
+      picname = "music_icon.png"
+    end
+    return path + picname
   end
 end
