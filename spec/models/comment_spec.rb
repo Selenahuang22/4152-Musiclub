0b7951ec279c982180b0bcf94d68acbe47f8bd37
Wannuo Sun <ws2591@columbia.edu>
2022-12-19 20:11:42 +0800

    Update spec for models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe Comment do
+  describe 'test self.with_music' do
+    let!(:comment1) { FactoryBot.create(:comment,music_id:'1') }
+      it 'should return comments' do
+        Comment.instance_variable_set(:@comments, comment1)
+        expect(Comment.with_music('1')).to match([:author=>'Default', :content=>"Comment Content"])
+      end
+  end
 end
